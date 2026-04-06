d1b70fc6eacd26967d7b88c4663630f5d35f8280
shreyan-naskar <shreyannaskar@gmail.com>
2026-04-06 10:49:25 +0530

    data prep file

---
+++ make-data.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import pandas as pd
+
+def process_target_csv(filepath: str, start_time: str = "2024-01-01"):
+    filepath = Path(filepath)
+    name = filepath.stem.replace("_target", "")
+
+    df = pd.read_csv(filepath, header=None)
+    # print(df.shape)
+    output_dir = Path("data") / name
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for col_number, (_, col) in enumerate(df.items()):        
+        values = col.values
+        timestamps = pd.date_range(start=start_time, periods=len(values), freq="h")
+
+        out_df = pd.DataFrame({"timestamp": timestamps, "target": values})
+        out_df.to_csv(output_dir / f"{col_number}.csv", index=False)
+
+if __name__ == "__main__":
+  data_dir = Path("data_code/data")
+  for csv_file in sorted(data_dir.glob("*.csv")):
+      process_target_csv(csv_file)
+    
